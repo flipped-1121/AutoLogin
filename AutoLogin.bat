de0f516e523cc46de2c3812ec75f76099b461a87
Chase1 <chaseone1@foxmail.com>
2023-10-01 16:11:43 +0800

    自动选择AUST_Student
    修补了不选择校园网WIFI的情况下链接命令无效的问题

--- AutoLogin.bat
+++ AutoLogin.bat
@@ -1,2 +1,3 @@
+netsh wlan connect name=AUST_Student
 curl -d "callback=dr1003&DDDDD=201930****@****&upass=*******&0MKKey=123456" http://10.255.0.19/drcom/login
 curl -d "callback=dr1003&DDDDD=201930****@****&upass=*******&0MKKey=123456" http://10.255.0.19/drcom/login
